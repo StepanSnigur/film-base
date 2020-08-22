da88cc7d918f6241ee63de20e0c38401a50b9b56
Stepan Snigur <snigur-stepan@mail.ru>
2020-08-22 16:52:36 +0300

    Fix tests

--- src/tests/PaginationBarTests/PaginationBar.test.js
+++ src/tests/PaginationBarTests/PaginationBar.test.js
@@ -1,25 +1,37 @@
-import React from 'react';
-import { shallow } from 'enzyme';
+import React from 'react'
+import { Provider } from 'react-redux'
+import configureStore from 'redux-mock-store'
+import { shallow, mount } from 'enzyme'
 
-import PaginationBar from '../../components/PaginationBar';
+import PaginationBar from '../../components/PaginationBar'
 
 describe('pagination bar', () => {
-    it('correct pagination render at the top of the list', () => {
-        for (let i = 1; i <= 10; i++) {
-            let pagination = shallow(<PaginationBar currentPage={i} maxPagesCount={100} />);
-            expect(pagination.find('div').children()).toHaveLength(i + 10);
-        }
-    })
-    it('correct pagination render in the middle of list', () => {
-        for (let i = 11; i <= 89; i++) {
-            let pagination = shallow(<PaginationBar currentPage={i} maxPagesCount={100} />);
-            expect(pagination.find('div').children()).toHaveLength(21);
-        }
-    })
-    it('correct pagination render at the end of the list', () => {
-        for (let i = 90; i <= 100; i++) {
-            let pagination = shallow(<PaginationBar currentPage={i} maxPagesCount={100} />);
-            expect(pagination.find('div').children()).toHaveLength(11 + (100 - i));
-        }
-    })
+  const createPaginationBar = page => {
+    return mount(
+      <Provider store={store}>
+        <PaginationBar currentPage={page} pagesCount={100} onPageChange={() => {}} />
+      </Provider>
+    )
+  }
+  const mockStore = configureStore([])
+  const store = mockStore({})
+
+  it('correct pagination render at the top of the list', () => {
+    for (let i = 1; i <= 10; i++) {
+      const pagination = createPaginationBar(i)
+      expect(pagination.find('div').find('div').children()).toHaveLength(i + 13)
+    }
+  })
+  it('correct pagination render in the middle of list', () => {
+    for (let i = 11; i <= 89; i++) {
+      const pagination = createPaginationBar(i)
+      expect(pagination.find('div').find('div').children()).toHaveLength(24)
+    }
+  })
+  it('correct pagination render at the end of the list', () => {
+    for (let i = 90; i <= 100; i++) {
+      const pagination = createPaginationBar(i)
+      expect(pagination.find('div').find('div').children()).toHaveLength(14 + (100 - i))
+    }
+  })
 })
